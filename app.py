0d1ddd44b66e87c1d952f02ca1ef7f7b06ab659d
AdityaV9033 <152848911+AdityaV9033@users.noreply.github.com>
2023-12-21 01:32:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('ClusterID of Countries for US Arrest Dataset')
-df = pd.read_csv('USArrests.csv',index_col=0)
+df = pd.read_csv('USArrests.csv')
 df.head()
-
+X = df.iloc[:,1:]
+X.head()
 from sklearn.cluster import KMeans
 model = KMeans(n_clusters=3,random_state=2021)
-model.fit(df)
-
+model.fit(X)
+Y = pd.read_csv('USArrests.csv',index_col=0)
+Y.head()
 
 st.sidebar.header("Select the ML model you want to use")
 Drop_options = ["K Means Clustering"]
 Model_choice = st.sidebar.selectbox("Drop_options", options=Drop_options)
 
 Index = st.slider("Country Name", min_value=0, max_value=49, step=1)
-st.text(f"Country Name: {df.index[Index]}")
+st.text(f"Country Name: {Y.index[Index]}")
 Murder = st.slider("Murder", min_value=0.8, max_value=17.4, step=0.1)
 st.text(f"Muder value: {Murder}")
 Assault = st.slider("Assault", min_value=45, max_value=337, step=1)
@@ -33,5 +35,5 @@ input={'murder':Murder,
 input_X=pd.DataFrame(input, index=['value'])
 st.text("Input value of features:")
 input_X.T
-ypred=model.predict(df)
-st.text(f"ClusterID of the country {df.index[Index]} is: {ypred[Index]}")
+ypred=model.predict(X)
+st.text(f"ClusterID of the country {Y.index[Index]} is: {ypred[Index]}")
