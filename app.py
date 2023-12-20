ae98d03687478f41baeb9dfffd62284818b90d06
AdityaV9033 <152848911+AdityaV9033@users.noreply.github.com>
2023-12-21 01:44:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ input_X=pd.DataFrame(input, index=['value'])
 st.text("Input value of features:")
 input_X.T
 ypred=model.predict(input_X)
-st.text(f"ClusterID of the country {Y.index[Index]} is: {ypred}")
+st.text(f"ClusterID of the country is: {ypred}")
